8557addea1e31b20ca5587bcb5a495134813f60e
Peter Egger <peter.pfaffeneder@gmail.com>
2024-10-18 23:14:16 +0200

    wip: try upgrade typst to latest dev version to get feature for CLI arguments

--- flake.nix
+++ flake.nix
@@ -2,15 +2,30 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/24.05";
     pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
   };
 
   outputs = { self, nixpkgs, pre-commit-hooks }: let
       system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = self.overlays.${system}.default;
+      };
   in
   {
+    overlays.${system}.default = [
+      (final: prev: {
+        typst = prev.typst.overrideAttrs (old: {
+          patches = old.patches ++ [
+            (prev.fetchpatch {
+              url = "https://github.com/typst/typst/commit/15a26c6c45bea9a539b0dfdb3999422d8ce1f558.patch";
+              hash = "sha256-ZQnONB0gB6SjMT5ixhPD0JlamfZNiEdophDLy3nFKH0=";
+            })
+          ];
+        });
+      })
+    ];
     checks.${system} = {
       pre-commit-check = pre-commit-hooks.lib.${system}.run {
         src = ./.;
