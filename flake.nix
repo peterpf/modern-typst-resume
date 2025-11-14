bfaf60a56fcfb15228a6932fc6b1c203b143d175
Peter Egger <peter.pfaffeneder@gmail.com>
2025-11-14 21:10:11 +0100

    style: format files

--- flake.nix
+++ flake.nix
@@ -7,12 +7,15 @@
     pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
   };
 
-  outputs = { self, nixpkgs, typst, pre-commit-hooks }:
-  let
+  outputs = {
+    self,
+    nixpkgs,
+    typst,
+    pre-commit-hooks,
+  }: let
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
-  in
-  {
+  in {
     checks.${system} = {
       pre-commit-check = pre-commit-hooks.lib.${system}.run {
         src = ./.;
@@ -25,13 +28,13 @@
       tests = pkgs.stdenv.mkDerivation {
         name = "test_lib";
         src = self;
-        buildInputs = [ pkgs.typst ];
-        buildPhase = ''bash
+        buildInputs = [pkgs.typst];
+        buildPhase = ''
           typst compile tests/test_lib.typ --root . --input config=tests/config.yaml >> log.txt
         '';
 
         unpackPhase = "";
-        installPhase = ''bash
+        installPhase = ''
           mkdir -p $out/bin
           mv log.txt $out/bin
         '';
@@ -40,9 +43,11 @@
 
     devShells.${system}.default = pkgs.mkShell {
       inherit (self.checks.${system}.pre-commit-check) shellHook;
-      packages = [
-        typst.packages.${system}.typst-dev
-      ] ++ self.checks.${system}.pre-commit-check.enabledPackages;
+      packages =
+        [
+          typst.packages.${system}.typst-dev
+        ]
+        ++ self.checks.${system}.pre-commit-check.enabledPackages;
     };
   };
 }
